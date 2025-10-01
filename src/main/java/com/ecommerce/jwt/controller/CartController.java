3f3386e191a885bdabbdc4b6f705f74eae73ea61
Devashree Lonkar <devashrilonkar2016@gmail.com>
2025-10-01 23:31:00 +0530

    cart changes

--- src/main/java/com/ecommerce/jwt/controller/CartController.java
+++ src/main/java/com/ecommerce/jwt/controller/CartController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ecommerce.jwt.dto.CartResponseDTO;
+import com.ecommerce.jwt.entity.Cart;
 import com.ecommerce.jwt.service.CartService;
 
 @RestController
@@ -16,8 +18,8 @@ public class CartController {
 	
 	@PreAuthorize("hasRole('User')")
 	@GetMapping({"/addToCart/{productId}"})
-	public void addToCart(@PathVariable(name = "productId") Long productId) {
-		
+	public CartResponseDTO addToCart(@PathVariable(name = "productId") Long productId) {
+		return cartService.addToCart(productId);
 	}
 
 }
